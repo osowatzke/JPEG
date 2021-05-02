9d1b228eaad6ab73fcca97ae9f1cf357f3976390
Christine Saw <78784280+csaw68@users.noreply.github.com>
2021-05-01 20:19:09 -0400

    quant+qual plots

---
+++ plot_mse_vs_q.m
@@ -0,0 +1,39 @@
+function plot_mse_vs_q(image_path,psnr_array,qf_array)
+
+% store MSE values
+mse_array = zeros(length(psnr_array),length(qf_array));
+
+for jj = 1:length(psnr_array)
+    for ii = 1:length(qf_array)
+
+        % maintain constant low noise
+        PSNR = psnr_array(jj);
+
+        % quality factor
+        qf = qf_array(ii);
+
+        % process image for various Q values 
+        [~, ~, mse, ~, ~, ~] = ...
+        im_process(image_path, PSNR, qf);
+
+        % store MSE for corresponding Q value
+        mse_array(jj,ii) = mse;
+
+    end
+end
+
+% plot MSE vs Q
+hold on;
+for ii = 1:length(psnr_array)
+    plot(qf_array,mse_array(ii,:),'DisplayName',['PSNR = ',num2str(psnr_array(ii))]);
+end
+grid on;
+xlabel('Quality Factor (Q)');
+ylabel('Mean Squared Error (MSE)');
+title('MSE vs Q');
+hold off;
+legend;
+if length(psnr_array) == 1
+    legend off;
+end
+end
---
+++ plots_generator.m
@@ -0,0 +1,28 @@
+function plots_generator()
+
+    % 'Finding the optimal Q value for images with different PSNR values'
+    % plot MSE vs Q at different PSNR settings
+    qf_array = [1,10:10:80]; % Q values
+    psnr_array = (10:10:50); % PSNR values
+    % returns plot of mse vs q
+    figure; plot_mse_vs_q(image_path,psnr_array,qf_array);
+    
+    % 'Observing MSE for various Q values'
+    % plot MSE vs Q for image at minimal noise 
+    qf_array = [1,10:10:80]; % Q values
+    figure; plot_mse_vs_q(image_path,80,qf_array);
+
+    % 'Given a noisy image, how well can the system remove the noise at 
+    % different Q values?'
+    qf_array = [1,10:10:80]; % set quality factor values
+    PSNR = 10; % set noise for noisy image
+    % currently only takes in qf_array size 9 to generate 3x3 subplot
+    plot_denoise(image_path,PSNR,qf_array);
+
+    % 'How much space can we save using different Q values?'
+    qf_array = [1,10:10:80]; % Q values
+    PSNR = 80; % set noise of image
+    % currently only takes in qf_array size 9 to generate 3x3 subplot
+    plot_space(image_path,PSNR,qf_array);
+
+end
